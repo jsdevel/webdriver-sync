99650a35c6b67765a66b2dd4f7f15789068238e7
joseph <joseph@JHP>
2013-10-21 01:34:07 -0700

    Finishing File

--- src/classes/File.js
+++ src/classes/File.js
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 var Class                   = require('../imports').File;
+var Long                    = require('../imports').Long;
 var Instance                = require('../classes/Instance');
 var assert                  = require('../assert');
 
@@ -55,75 +56,203 @@ File.pathSeparatorChar = File.prototype.pathSeparatorChar = Class.pathSeparatorC
 File.separator         = File.prototype.separator         = Class.separator;
 File.separatorChar     = File.prototype.separatorChar     = Class.separatorChar;
 
-//TODO finsh
-//METHODS
-
-
 File.prototype.canExecute=function(){
    return this._instance.canExecuteSync();
 };
+
 File.prototype.canRead=function(){
    return this._instance.canReadSync();
 };
+
 File.prototype.canWrite=function(){
    return this._instance.canWriteSync();
 };
+
 File.prototype.compareTo=function(pathname){
    assert(pathname).isInstanceof(File).throws(
       "Pathname must be an instance of File."
    );
    return this._instance.compareToSync(pathname);
 };
+
 File.prototype.createNewFile=function(){
    return this._instance.createNewFileSync();
 };
-File.prototype.createTempFile=function(prefix, suffix){
 
-   assert(pathname).isInstanceof(File).throws(
-      "Pathname must be an instance of File."
-   );
-   return this._instance.compareToSync(pathname);
-   555asdf
+File.createTempFile=File.prototype.createTempFile=function(
+   prefix,
+   suffix,
+   directory
+){
+   assert(prefix).isString().throws("Prefix must be a string.  Found: "+prefix);
+   assert(suffix).isString().throws("Suffix must be a string.  Found: "+suffix);
+   if(arguments.length === 3){
+      assert(pathname).isInstanceof(File).throws(
+         "The directory must be an instance of File"
+      );
+      return new File(new Instance(this._instance.createTempFileSync(
+         prefix,
+         suffix,
+         directory._instance
+      )));
+   }
+   return new File(new Instance(this._instance.createTempFileSync(
+      prefix,
+      suffix
+   )));
 };
-static File	createTempFile(String prefix, String suffix)
-static File	createTempFile(String prefix, String suffix, File directory)
-boolean	delete()
-void	deleteOnExit()
-boolean	equals(Object obj)
-boolean	exists()
-File	getAbsoluteFile()
-String	getAbsolutePath()
-File	getCanonicalFile()
-String	getCanonicalPath()
-long	getFreeSpace()
-String	getName()
-String	getParent()
-File	getParentFile()
-String	getPath()
-long	getTotalSpace()
-long	getUsableSpace()
-int	hashCode()
-boolean	isAbsolute()
-boolean	isDirectory()
-boolean	isFile()
-boolean	isHidden()
-long	lastModified()
-long	length()
-String[]	list()
-String[]	list(FilenameFilter filter)
-File[]	listFiles()
-File[]	listFiles(FileFilter filter)
-File[]	listFiles(FilenameFilter filter)
-static File[]	listRoots()
-boolean	mkdir()
-boolean	mkdirs()
-boolean	renameTo(File dest)
-boolean	setExecutable(boolean executable)
-boolean	setExecutable(boolean executable, boolean ownerOnly)
-boolean	setLastModified(long time)
-boolean	setReadable(boolean readable)
-boolean	setReadable(boolean readable, boolean ownerOnly)
-boolean	setReadOnly()
-boolean	setWritable(boolean writable)
-boolean	setWritable(boolean writable, boolean ownerOnly)
-String	toString()
+
+File.prototype.delete=function(){
+   return this._instance.deleteSync();
+};
+
+File.prototype.deleteOnExit=function(){
+   return this._instance.deleteOnExitSync();
+};
+
+
+File.prototype.equals=function(obj){
+   if(obj && "_instance" in obj){
+      return this._instance.equalsSync(obj._instance);
+   }
+   return false;
+};
+
+File.prototype.exists=function(){
+   return this._instance.existsSync();
+};
+
+File.prototype.getAbsoluteFile=function(){
+   return new File(new Instance(this._instance.getAbsoluteFileSync()));
+};
+
+File.prototype.getAbsolutePath=function(){
+   return this._instance.getAbsolutePathSync();
+};
+
+File.prototype.getCanonicalFile=function(){
+   return new File(new Instance(this._instance.getCanonicalFileSync()));
+};
+
+File.prototype.getCanonicalPath=function(){
+   return this._instance.getCanonicalPathSync();
+};
+
+File.prototype.getFreeSpace=function(){
+   return this._instance.getFreeSpaceSync();
+};
+
+File.prototype.getName=function(){
+   return this._instance.getNameSync();
+};
+
+File.prototype.getParent=function(){
+   return this._instance.getParentSync();
+};
+
+File.prototype.getParentFile=function(){
+   return new File(new Instance(this._instance.getParentFileSync()));
+};
+
+File.prototype.getPath=function(){
+   return this._instance.getPathSync();
+};
+
+File.prototype.getTotalSpace=function(){
+   return this._instance.getTotalSpaceSync();
+};
+
+File.prototype.getUsableSpace=function(){
+   return this._instance.getUsableSpaceSync();
+};
+
+File.prototype.isAbsolute=function(){
+   return this._instance.isAbsoluteSync();
+};
+
+File.prototype.isDirectory=function(){
+   return this._instance.isDirectorySync();
+};
+
+File.prototype.isFile=function(){
+   return this._instance.isFileSync();
+};
+
+File.prototype.isHidden=function(){
+   return this._instance.isHiddenSync();
+};
+
+File.prototype.lastModified=function(){
+   return this._instance.lastModifiedSync();
+};
+
+File.prototype.length=function(){
+   return this._instance.lengthSync();
+};
+
+File.prototype.list=function(){
+   return this._instance.listSync();
+};
+
+File.prototype.listFiles=function(){
+   return this._instance.listFilesSync().map(function(v){
+      return new File(new Instance(v));
+   });
+};
+
+File.prototype.listRoots=function(){
+   return this._instance.listRootsSync().map(function(v){
+      return new File(new Instance(v));
+   });
+};
+
+File.prototype.mkdir=function(){
+   return this._instance.mkdirSync();
+};
+
+File.prototype.mkdirs=function(){
+   return this._instance.mkdirsSync();
+};
+
+File.prototype.renameTo=function(dest){
+   assert(dest).isInstanceof(File).throws(
+      "Destination must be an instance of File."
+   );
+   return this._instance.renameToSync(dest._instance);
+};
+
+File.prototype.setExecutable=function(executable, ownerOnly){
+   if(assert.isBool(ownerOnly)){
+      return this._instance.setExecutableSync(!!executable, !!ownerOnly);
+   } else {
+      return this._instance.setExecutableSync(!!executable);
+   }
+};
+
+File.prototype.setLastModified=function(time){
+   return this._instance.setLastModifiedSync(new Long(time));
+};
+
+File.prototype.setReadable=function(readable, ownerOnly){
+   if(assert.isBool(ownerOnly)){
+      return this._instance.setReadableSync(!!readable, !!ownerOnly);
+   } else {
+      return this._instance.setReadableSync(!!readable);
+   }
+};
+
+File.prototype.setReadOnly=function(){
+   return this._instance.setReadOnlySync();
+};
+
+File.prototype.setWritable=function(writable, ownerOnly){
+   if(assert.isBool(ownerOnly)){
+      return this._instance.setWritableSync(!!writable, !!ownerOnly);
+   } else {
+      return this._instance.setWritableSync(!!writable);
+   }
+};
+
+File.prototype.toString=function(){
+   return this._instance.toStringSync();
+};
