46fae17ccf77e516f1dcf856ff8baf57835eb18c
joseph <joseph@JHP>
2013-10-22 00:09:02 -0700

    updating package.json, temporarily moving WebDriverWait

--- src/classes/WebDriverWait.js
+++ src/classes/WebDriverWait.js
@@ -1,11 +1,11 @@
-/*!
- * Copyright 2013 Joseph Spencer
+/*
+ * Copyright 2013 Joseph Spencer.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,12 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var imports           = require('./imports');
-var utils             = require('./utils');
-var classes           = imports.classes;
-var collectionToArray = utils.collectionToArray;
-
-
 
 
 function assertIsWebDriver(driver){
@@ -88,48 +82,3 @@ function WebDriverWait(driver, timeout, sleep){
       return wait.withMessageSync(message);
    };
 }
-
-
-var FirefoxDriver=(function(){
-   function FirefoxDriver(){
-      var driver = new FirefoxDriverClass();
-      Object.defineProperty(this, '_driver', {
-         get:function(){return driver;}
-      });
-   }
-   extend(FirefoxDriver, WebDriver);
-   return FirefoxDriver;
-})();
-
-var HtmlUnitDriver=(function(){
-   function HtmlUnitDriver(){
-      var driver = new HtmlUnitDriverClass();
-      Object.defineProperty(this, '_driver', {
-         get:function(){return driver;}
-      });
-   }
-   extend(HtmlUnitDriver, WebDriver);
-   return HtmlUnitDriver;
-})();
-
-module.exports={
-   ChromeDriver:ChromeDriver,
-   FirefoxDriver:FirefoxDriver,
-   HtmlUnitDriver:HtmlUnitDriver,
-   By:new By(),
-   ExpectedConditions:new ExpectedConditions(),
-   WebDriverWait:WebDriverWait,
-   Credentials:UserAndPassword,
-   Cookie:Cookie,
-   TimeUnits:TimeUnits,
-   /**
-    * @param {number} amount in mills to sleep for.
-    */
-   sleep:function(amount){
-      java.callStaticMethodSync(
-         "java.lang.Thread",
-         "sleep",
-         new Long(amount)
-      );
-   }
-};
