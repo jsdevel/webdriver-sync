6b7a02dbf0d8c06fc6ef466e70f0428781caf1d4
Joseph Spencer <js.developer.undefined@gmail.com>
2013-11-12 01:49:51 -0700

    closes #23: Implement SafariDriver enhancement.

---
+++ src/classes/SafariDriver.js
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2013 Joseph Spencer.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+var Class                   = require('../imports').SafariDriver;
+var Capabilities            = require('../interfaces/Capabilities');
+var TakesScreenshot         = require('../interfaces/TakesScreenshot');
+var RemoteWebDriver         = require('./RemoteWebDriver');
+var extendAll               = require('../utils').extendAll;
+var addFinalProp            = require('../utils').addFinalProp;
+
+module.exports=SafariDriver;
+
+extendAll(
+   SafariDriver,
+   TakesScreenshot,
+   RemoteWebDriver
+);
+
+//TODO finish constructor arguments
+function SafariDriver(capabilities){
+   var instance;
+   var len=arguments.length;
+
+   if(!len){
+      instance = new Class();
+   } else if(len === 1){
+         assert(capabilities)
+            .isInstanceof(Capabilities)
+            .throws(
+            "The first argument wasn't an instanceof Capabilities"
+         );
+         instance = new Class(capabilities._instance);
+   } else {
+      throw new Error("The wrong number of arguments was given.");
+   }
+
+   addFinalProp(this, "_instance", instance);
+}
