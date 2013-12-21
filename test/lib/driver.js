a131ba21ebf8839d1a9b45ded81efaa689a08321
jsdevel <js.developer.undefined@gmail.com>
2013-12-21 11:54:52 -0700

    debug info for travis

--- test/lib/driver.js
+++ test/lib/driver.js
@@ -13,6 +13,7 @@ var serviceBuilder;
 var service;
 
 if(process.env.TRAVIS){
+  console.log("Hello Travis!");
   service = new ChromeDriverService.Builder()
     .usingDriverExecutable(new File(findsChromeDriver.find()))
     .withEnvironment({"DISPLAY":":99.0"})
