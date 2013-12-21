3f7377569a52e537e8f501579e80ba3dd30cb87e
jsdevel <js.developer.undefined@gmail.com>
2013-12-21 06:20:15 -0700

    travis

--- test/lib/driver.js
+++ test/lib/driver.js
@@ -3,14 +3,18 @@ var webdriver = require(
   path.resolve(__dirname, '..', '..', 'src', 'webdriver-sync')
 );
 var DesiredCapabilities = webdriver.DesiredCapabilities;
+var ChromeDriver = webdriver.ChromeDriver;
+var ChromeOptions = webdriver.ChromeOptions;
 var PhantomJSDriver = webdriver.PhantomJSDriver;
 var RemoteWebDriver = webdriver.RemoteWebDriver;
+var chromeOptions = new ChromeOptions();
+chromeOptions.addArguments("--no-sandbox");
 //var service = webdriver.ChromeDriverService.createDefaultService();
 //service.start();
 
 module.exports = {
   get driver() {
-    return new PhantomJSDriver();
+    return new ChromeDriver();
     //temporary because travis build is failing due to environment issue
     //return new RemoteWebDriver(service.getUrl(), 
     //DesiredCapabilities.chrome());
